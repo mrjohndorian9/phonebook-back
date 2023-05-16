dea7f8fca49e8cf206ba16f0bdfa3fb07b421ddb
mrjohndorian9 <mrjohndorian9@gmail.com>
2023-05-16 23:42:36 +0600

    add frontend

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('dist'));
 
 morgan.token('body', function (req, res) {
   return JSON.stringify(req.body);
